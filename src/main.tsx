45c5a6d46e93457e2126503a0257953978c7c283
Sho Yasugami <vitamin.sho@gmail.com>
2024-04-24 20:11:35 +0900

    remove unused component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = createRoot(document.getElementById("root") as Element);
 
