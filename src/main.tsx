3c953a2c6bc19b03ab9bd976e2ef1c8b9be6fb75
Sho Yasugami <vitamin.sho@gmail.com>
2024-04-24 20:09:23 +0900

    dark modeが聞かないので一旦削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import {
-  ChakraProvider,
-  extendTheme,
-  ColorModeScript,
-  type ThemeConfig,
-} from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 const root = createRoot(document.getElementById("root") as Element);
 
-const config: ThemeConfig = {
-  initialColorMode: "dark",
-  useSystemColorMode: true,
-};
-
-const theme = extendTheme({ config });
-
 root.render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript />
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </StrictMode>
